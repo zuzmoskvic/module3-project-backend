8830373fdd0b824149f119cbe604d89c9633f4a2
“Zuzana <zuzana@getyourguide.com>
2023-07-02 07:13:00 +0200

    Saturday code - getting 500 error

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -12,23 +12,14 @@ const { Configuration, OpenAIApi, TranscriptionsApi } = require('openai');
 const FormData = require('form-data');
 const path = require('path');
 
-
 router.post("/signup", async (req, res) => {
-  
     const saltRounds = 13;
     const salt = bcrypt.genSaltSync(saltRounds);
     const hash = bcrypt.hashSync(req.body.password, salt);
     const newUser = await User.create({ email: req.body.email, password: hash });
     console.log("here is our new user in the DB", newUser);
-   
     res.status(201).json(newUser);
-
-  
-  
-  
-  
 });
-
 
 //login route
 router.post("/login", async (req, res) => {
@@ -72,10 +63,15 @@ router.get("/verify", enrichRequestWithUser, (req, res) => {
 });
 
 
+const { pipeline } = require('stream');
+const { promisify } = require('util');
+const streamifier = require('streamifier');
+const pipelineAsync = promisify(pipeline);
+
 
 router.post('/addRecord', enrichRequestWithUser, uploader.single("recordPath"), async (req, res, next) => {
   try {
-    
+    // Take record from the form and upload it to mongoose 
     const record = new Record({
       title: req.body.title,
       recordPath: req.file.path,
@@ -87,48 +83,69 @@ router.post('/addRecord', enrichRequestWithUser, uploader.single("recordPath"), 
       req.payload._id,
       { $push: { records: record._id } },
       { new: true }
-    )
+    );
 
-   // const filePath = path.join(__dirname, "audio.mp3");
+    // Method - using an audio stream 
+    /*
+    const audioStream = await axios.get(req.file.path, { responseType: 'stream' });
+
+    const formData = new FormData();
+    formData.append('audio', audioStream.data);
+
+    const response = await axios.post('https://api.openai.com/v1/audio/transcriptions', formData, {
+      headers: {
+        'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`,
+        'Content-Type': `multipart/form-data; boundary=${formData._boundary}`,
+      },
+    });
+
+    const transcript = response.data[0]?.text;
+    console.log('Transcript:', transcript);
+
+
+    // Check if the uploaded file is being received correctly
+    if (!req.file) {
+      return res.status(400).json({ error: 'No file uploaded' });
+    }
+
+    */
+    
+    // Metohod - using fs to create a readable stream
     const model = "whisper-1";
     const formData = new FormData();
     formData.append("model", model);
 
-
-    const createReadableStreamFromUrl = async (url) => {
-      const response = await axios.get(url, { responseType: 'stream' });
-      return response.data;
-    }; 
-
-    const remoteUrl = req.file.path;
-    console.log("",record);
-
-    const readStream = createReadableStreamFromUrl(remoteUrl);
-
+    const localFilePath = './temp.wav' // Local file path to save the downloaded file
+    const response = await axios.get(req.file.path, { responseType: 'stream' });
+    await pipelineAsync(response.data, fs.createWriteStream(localFilePath));
+    const readStream = fs.createReadStream(localFilePath);
     formData.append("recordPath", readStream);
 
     axios.post("https://api.openai.com/v1/audio/transcriptions", formData, 
       {
         headers: {
           ...formData.getHeaders(),
-          'authorization': `Bearer ${process.env.OPENAI_API_KEY}`,
+          'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`,
           "Content-Type": `multipart/form-data; boundary=${formData._boundary}`
         },
       })
-      .then((response)=> {
-        console.log(response.data);
+      .then((response) => {
+        console.log("This is the response.data", response.data)
       });
 
-   
+    // Cleanup: Delete the temporary local file
+    fs.unlinkSync(localFilePath);
+    res.status(201).json(record);
   } catch (err) {
     console.error(err);
     // Handle the error appropriately
     res.status(500).json({ error: 'An error occurred' });
   }
-});
+})
 
 
-
+// enrichRequestWithPrivateThings middleware 
+/*
 const enrichRequestWithPrivateThings = async (req, res, next) => {
   const { _id } = req.payload;
   try {
@@ -139,12 +156,12 @@ const enrichRequestWithPrivateThings = async (req, res, next) => {
   } catch (err) {
     console.log(err);
   }
-};
+};*/
 
 router.get(
   "/private-page",
   enrichRequestWithUser,
-  enrichRequestWithPrivateThings,
+  //enrichRequestWithPrivateThings,
   async (req, res) => {
     res.status(200).json({ privateThings: req.privateThings });
   }
@@ -153,7 +170,7 @@ router.get(
 router.get(
   "/private-page-2",
   enrichRequestWithUser,
-  enrichRequestWithPrivateThings,
+  //enrichRequestWithPrivateThings,
 
   async (req, res) => {
     res.status(200).json({ privateThings: req.privateThings });
