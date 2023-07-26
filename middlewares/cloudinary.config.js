75cef00a859dce5533650a65ee5fa7d581624ff1
“Zuzana <zuzana@getyourguide.com>
2023-07-26 23:38:25 +0200

    Small middleware cleanup

--- middlewares/cloudinary.config.js
+++ middlewares/cloudinary.config.js
@@ -13,13 +13,14 @@ cloudinary.config({
 const storage = new CloudinaryStorage({
   cloudinary,
   params: {
-    folder: 'bananarama', // Specify the folder where you want to store the uploaded files in your Cloudinary account
-    allowedFormats: ['mp4', 'm4a', 'mp3', 'wav', 'mpeg'], // Specify the allowed file formats
-    resource_type: 'auto', // Automatically detect the resource type (image, video, raw)
+    folder: 'bananarama',
+    allowedFormats: ['mp4', 'm4a', 'mp3', 'wav', 'mpeg'], 
+    resource_type: 'auto',
   },
 });
 
 // Create the multer instance with the CloudinaryStorage configuration
 const cloudinaryAudioUploader = multer({ storage });
+
 
 module.exports = cloudinaryAudioUploader;
